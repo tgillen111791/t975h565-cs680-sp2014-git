836d92b5323541072603d9289a7390245bfc91aa
Tyler Gillen <tgillen111791@gmail.com>
2014-02-25 09:52:49 -0600

    Initial import

---
+++ hello.c
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1265
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs24 \cf0 /* Hello World program */\
+\
+#include<stdio.h>\
+\
+main()\
+\{\
+    printf("Hello World");\
+\
+\}\
+}
